01b13ca33f0186853cc88bff5952859217b7e966
hoshyar Mahmoudi <hoshicameron@gmail.com>
2024-07-08 21:14:26 +0330

    Feat test enemy health

--- Assets/DefenseNetwork/Modules/EnemiesModule/Scripts/Tests/EnemyTest.cs
+++ Assets/DefenseNetwork/Modules/EnemiesModule/Scripts/Tests/EnemyTest.cs
@@ -1,18 +1,31 @@
 using System;
 using System.Collections.Generic;
+using DefenseNetwork.Core.EventChannels.DataObjects;
+using GameSystemsCookbook;
 using UnityEngine;
 
 namespace DefenseNetwork.Modules.EnemiesModule.Scripts.Tests
 {
     public class EnemyTest : MonoBehaviour
     {
+        [Header("Event Channel")] 
+        [SerializeField] private HitEventChannelSO hitEventChannelSo;
         [SerializeField] private List<Vector3> path;
         [SerializeField] private Enemy enemyPrefab;
 
+        private Enemy enemy;
         private void Start()
         {
-            var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             enemy.Initialize(path);
+        }
+
+        private void Update()
+        {
+            if (enemy.gameObject != null)   return;
+            
+            if(Input.GetKeyDown(KeyCode.Space))
+                hitEventChannelSo.RaiseEvent(new HitDTO { hittedObject = enemy.gameObject, damage = 2 });
         }
     }
 }
